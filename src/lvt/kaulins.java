4613415d2c06270a9fe765e7635d657188819a7e
Keis <keisija.liepina@gmail.com>
2025-09-02 12:11:30 +0300

    Uzsākta projekta versionēšana

---
+++ src/lvt/kaulins.java
@@ -0,0 +1,15 @@
+package lvt;
+
+import java.util.Random;
+
+public class kaulins {
+
+	public static void main(String[] args) {
+		int skaitlis;
+		
+		Random rand = new Random();
+		skaitlis = rand.nextInt(6)+1;
+		System.out.println("Uzkrita skaitlis " + skaitlis);
+	}
+
+}
