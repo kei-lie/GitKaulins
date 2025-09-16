4b978263a31ae81902151beb278e140ed5697349
kei-lie <keisija.liepina@gmail.com>
2025-09-16 09:12:35 +0300

    Pievienots paziņojums

--- src/lvt/kaulins.java
+++ src/lvt/kaulins.java
@@ -1,15 +1,25 @@
 package lvt;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class kaulins {
-
-	public static void main(String[] args) {
-		int skaitlis;
-		
+	
+	static void mestKaulinu(int reizes){
+		int sk;
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6)+1;
-		System.out.println("Uzkrita skaitlis " + skaitlis);
+		for (int i = 1; i < reizes; i++) {
+		sk = rand.nextInt(6)+1; // ieskseejaa pieskaita par +1
+		System.out.println("Uzkrita skaitlis: " + sk);
+		}
+	}
+	public static void main(String[] args) {
+		int reizes;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cik reizes mest kauliņu?");
+		reizes = scan.nextInt();
+		mestKaulinu(reizes);
+		scan.close();
 	}
 
 }
